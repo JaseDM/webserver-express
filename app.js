d880126530bed41418ce4d90341c37337a9fe4bd
JaseDm <josemi.fotografo@gmail.com>
2020-11-26 11:02:22 +0100

    Ejercicio de prueba de aplicación con Node

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+ 
+
+
+
+app.use(express.static( __dirname + '/public'));
+
+hbs.registerPartials( __dirname + '/views/parciales', function (err) {});
+app.set('view engine', 'hbs');
+
+
+ 
+app.get('/', function (req, res) {
+  
+  res.render('home', {
+    nombre: 'Jose Miguel',
+    anio: new Date().getFullYear()
+  });
+
+});
+
+app.get('/about', function (req, res) {
+    
+    res.render('about', {
+      anio: new Date().getFullYear()
+    });
+    
+  })
+ 
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
---
+++ hbs/helpers.js
@@ -0,0 +1,8 @@
+const hbs = require('hbs');
+
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+  });
+
+
+
